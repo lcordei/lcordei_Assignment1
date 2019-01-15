8a416790992ec6b580644bce91c9a327a9188f3f
lcordei <lcordei@uwo.ca>
2019-01-15 00:10:39 -0500

    Part 3&4

---
+++ Assets/__Scripts/ChangeAngleByClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAngleByClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        
+            gameObject.transform.rotation = Quaternion.Euler(Random.value*20, Random.value*20, Random.value*-20);
+        
+    }
+}
---
+++ Assets/__Scripts/ResetByClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetByClick : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    private Rigidbody rb;
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            rb = GetComponent<Rigidbody>();
+            gameObject.transform.position = new Vector3(x, y, z);
+            rb.velocity = new Vector3(0f, 0f, 0f);
+            rb.angularVelocity = new Vector3(0f, 0f, 0f);
+            rb.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+
+    
+}
