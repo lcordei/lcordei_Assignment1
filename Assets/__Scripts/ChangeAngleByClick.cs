5fa54c63d7fae9c2c6135b5cd36a01d397d2fd1e
lcordei <lcordei@uwo.ca>
2019-01-16 19:11:06 -0500

    Revision 1

--- Assets/__Scripts/ChangeAngleByClick.cs
+++ Assets/__Scripts/ChangeAngleByClick.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ChangeAngleByClick : MonoBehaviour
 {
+    private Quaternion newAngle;
+
     void OnMouseDown()
     {
-        
-            gameObject.transform.rotation = Quaternion.Euler(Random.value*20, Random.value*20, Random.value*-20);
-        
+        newAngle = Quaternion.Euler(Random.value * 20, Random.value * 20, Random.value * -20);
+    }
+
+    void Update()
+    {
+        if(transform.rotation!= newAngle)
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, newAngle, 0.1f);
     }
 }
